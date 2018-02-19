8b34aaf7dacf79acce72aa48cebe6f4168338a6d
Nandigam Vijay Sai <vijaysai88nandigam@gmail.com>
2018-02-19 20:49:10 +0530

    Fixed the primary color of the single select

--- src/components/doughnut/doughnut.ts
+++ src/components/doughnut/doughnut.ts
@@ -71,7 +71,7 @@ export class DoughnutComponent {
         verticalAlign: 'middle',
         style:this.isSelectedGraph ?{fontWeight:'bold'}:{fontWeight:'normal'},
         y: 8,
-        x: 4
+        x: 0
       },
       tooltip: {
         enabled: false
--- src/pages/alerts/alerts.ts
+++ src/pages/alerts/alerts.ts
@@ -100,39 +100,42 @@ export class AlertsPage {
   }
 
   ionViewDidLoad() {
-    this.airtable.getAlerts().subscribe(data => {
-      this.inputAlerts = data['records'];
-
-      var drillingProp = this.inputAlerts.find(o => o.fields["Name"] === 'Drilling');
-      var trippingOutProp = this.inputAlerts.find(o => o.fields["Name"] === 'Tripping Out');
-      var trippingInProp = this.inputAlerts.find(o => o.fields["Name"] === 'Tripping In');
-      var equipFirstProp = this.inputAlerts.find(o => o.fields["Name"] === 'Placeholder 1');
-      var equipSecondProp = this.inputAlerts.find(o => o.fields["Name"] === 'Placeholder 2');
-      var equipThirdProp = this.inputAlerts.find(o => o.fields["Name"] === 'Placeholder 3');
-
-      this.alertsData[0].process.drilling = drillingProp.fields["# of Alerts last 12 Hours"];
-      this.alertsData[0].process.trippingOut = trippingOutProp.fields["# of Alerts last 12 Hours"];
-      this.alertsData[0].process.trippingIn = trippingInProp.fields["# of Alerts last 12 Hours"];
-      this.alertsData[0].process.total = this.alertsData[0].process.drilling + this.alertsData[0].process.trippingOut + this.alertsData[0].process.trippingIn;
-      this.alertsData[0].equipment.maintainance = equipFirstProp.fields["# of Alerts last 12 Hours"];
-      this.alertsData[0].equipment.total = this.alertsData[0].equipment.maintainance;
-      this.alertsData[0].total = this.alertsData[0].process.total + this.alertsData[0].equipment.total;
-      this.alertsData[0].unacknowledged = drillingProp.fields["# of Alerts unAcknowledged last 12 H"] + trippingOutProp.fields["# of Alerts unAcknowledged last 12 H"] + trippingInProp.fields["# of Alerts unAcknowledged last 12 H"];
-
-      this.alertsData[1].process.drilling = drillingProp.fields["# of Alerts last 24 Hours"];
-      this.alertsData[1].process.trippingOut = trippingOutProp.fields["# of Alerts last 24 Hours"];
-      this.alertsData[1].process.trippingIn = trippingInProp.fields["# of Alerts last 24 Hours"];
-      this.alertsData[1].process.total = this.alertsData[1].process.drilling + this.alertsData[1].process.trippingOut + this.alertsData[1].process.trippingIn;
-      this.alertsData[1].equipment.maintainance = equipFirstProp.fields["# of Alerts last 24 Hours"];
-      this.alertsData[1].equipment.total = this.alertsData[1].equipment.maintainance;
-      this.alertsData[1].total = this.alertsData[1].process.total + this.alertsData[1].equipment.total;
-      this.alertsData[1].unacknowledged = drillingProp.fields["# of Alerts unAcknowledged last 24 H"] + trippingOutProp.fields["# of Alerts unAcknowledged last 24 H"] + trippingInProp.fields["# of Alerts unAcknowledged last 24 H"];
-      this.drawGraph = true;
-    })
+    this.getData();
   }
+getData(){
+  this.airtable.getAlerts().subscribe(data => {
+  this.inputAlerts = data['records'];
 
+  var drillingProp = this.inputAlerts.find(o => o.fields["Name"] === 'Drilling');
+  var trippingOutProp = this.inputAlerts.find(o => o.fields["Name"] === 'Tripping Out');
+  var trippingInProp = this.inputAlerts.find(o => o.fields["Name"] === 'Tripping In');
+  var equipFirstProp = this.inputAlerts.find(o => o.fields["Name"] === 'Placeholder 1');
+  var equipSecondProp = this.inputAlerts.find(o => o.fields["Name"] === 'Placeholder 2');
+  var equipThirdProp = this.inputAlerts.find(o => o.fields["Name"] === 'Placeholder 3');
+
+  this.alertsData[0].process.drilling = drillingProp.fields["# of Alerts last 12 Hours"];
+  this.alertsData[0].process.trippingOut = trippingOutProp.fields["# of Alerts last 12 Hours"];
+  this.alertsData[0].process.trippingIn = trippingInProp.fields["# of Alerts last 12 Hours"];
+  this.alertsData[0].process.total = this.alertsData[0].process.drilling + this.alertsData[0].process.trippingOut + this.alertsData[0].process.trippingIn;
+  this.alertsData[0].equipment.maintainance = equipFirstProp.fields["# of Alerts last 12 Hours"];
+  this.alertsData[0].equipment.total = this.alertsData[0].equipment.maintainance;
+  this.alertsData[0].total = this.alertsData[0].process.total + this.alertsData[0].equipment.total;
+  this.alertsData[0].unacknowledged = drillingProp.fields["# of Alerts unAcknowledged last 12 H"] + trippingOutProp.fields["# of Alerts unAcknowledged last 12 H"] + trippingInProp.fields["# of Alerts unAcknowledged last 12 H"];
+
+  this.alertsData[1].process.drilling = drillingProp.fields["# of Alerts last 24 Hours"];
+  this.alertsData[1].process.trippingOut = trippingOutProp.fields["# of Alerts last 24 Hours"];
+  this.alertsData[1].process.trippingIn = trippingInProp.fields["# of Alerts last 24 Hours"];
+  this.alertsData[1].process.total = this.alertsData[1].process.drilling + this.alertsData[1].process.trippingOut + this.alertsData[1].process.trippingIn;
+  this.alertsData[1].equipment.maintainance = equipFirstProp.fields["# of Alerts last 24 Hours"];
+  this.alertsData[1].equipment.total = this.alertsData[1].equipment.maintainance;
+  this.alertsData[1].total = this.alertsData[1].process.total + this.alertsData[1].equipment.total;
+  this.alertsData[1].unacknowledged = drillingProp.fields["# of Alerts unAcknowledged last 24 H"] + trippingOutProp.fields["# of Alerts unAcknowledged last 24 H"] + trippingInProp.fields["# of Alerts unAcknowledged last 24 H"];
+  this.drawGraph = true;
+})
+}
 
   durationChange(value: string) {
+    this.getData();
     if (value === "last12H") {
       this.currentAlertsData = this.alertsData[0]
       this.doughnuts.forEach(doughnut =>
